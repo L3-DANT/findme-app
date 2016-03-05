72c698179e7f88bc9ec7363a3d2bef580c803577
Maxime Signoret <maxime.signoret@gmail.com>
2016-03-05 14:57:45 +0100

    [master] Add single app

---
+++ findme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  findme
+//
+//  Created by Maxime Signoret on 05/03/16.
+//  Copyright © 2016 Maxime Signoret. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
