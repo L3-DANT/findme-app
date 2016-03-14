2c8d1ce98fd40c03b0d7ec8c7b65132015d719ef
Nico Plop <nyckoo@live.fr>
2016-03-14 21:52:18 +0100

    zoom automatiquement sur la position courante au demarage

--- findme/ViewController.swift
+++ findme/ViewController.swift
@@ -68,13 +68,13 @@ class ViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDe
     
     //fonction appelée a chaque refresh de la location utilisée pour recentrer la caméra sur l'utilisateur automatiquement
     
-    /*func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations.last
         let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
         self.mapView.setRegion(region , animated: true )
         self.locationManager.stopUpdatingLocation()
-    }*/
+    }
     
     //fonction appelée en cas d'erreur
     
