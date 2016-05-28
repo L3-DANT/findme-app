ca53b9125d60db85b6591e612cd71cf7ff4f8c4f
Maxime Signoret <maxime.signoret@gmail.com>
2016-05-28 20:51:53 +0200

    [refacto] feat: Add user state attribute

--- findme/Model/User.swift
+++ findme/Model/User.swift
@@ -10,15 +10,22 @@ import Foundation
 
 
 class User {
-    var pseudo : String = ""
-    var longitude : Double = 0.0
-    var latitude : Double = 0.0
-    var friendList : [User]?
-    var phoneNumber : String = "000000000"
+    var pseudo: String = ""
+    var longitude: Double = 0.0
+    var latitude: Double = 0.0
+    var friendList: [User]?
+    var phoneNumber: String = "000000000"
+    var state: State = State.OFFLINE
+    
+    enum State : String {
+        case ONLINE
+        case AWAY
+        case OFFLINE
+    }
 
     init() {}
 
-    init(pseudo : String, latitude : Double, longitude : Double, friendList :[User] = [], phoneNumber : String) {
+    init(pseudo: String, latitude: Double, longitude: Double, phoneNumber: String, state: State, friendList: [User] = []) {
         self.pseudo = pseudo
         self.longitude = longitude
         self.latitude = latitude
