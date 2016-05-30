1094c5ce36dd82cdfaec91183465746f49cd1c83
Maxime Signoret <maxime.signoret@gmail.com>
2016-05-30 20:09:18 +0200

    [pusher] FEAT: deals with pusher

--- findme/Service/UserAnnotation.swift
+++ findme/Service/UserAnnotation.swift
@@ -21,7 +21,7 @@ class UserAnnotation : MKPointAnnotation {
         self.pinCustomImageName = "customPin"
     }
     
-    func updateCoordinate(newCoordinate : CLLocationCoordinate2D){
-        self.coordinate = coordinate
+    func updateCoordinate(newCoordinate : CLLocationCoordinate2D) {
+        self.coordinate = newCoordinate
     }
 }
